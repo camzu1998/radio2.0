146093567f890984dec3255ac6e7f6d28e0921a0
camzu1998 <kamillanger4@gmail.com>
2017-03-08 16:01:52 +0100

    GET REQUEST change

--- dodajCBG.php
+++ dodajCBG.php
@@ -6,7 +6,7 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 if ($polaczenie->connect_errno!=0){
 echo "Error: ".$polaczenie->connect_errno;
 } else {
-    $nazwa = $_GET["Nazwa"];
+    $nazwa = $_REQUEST["Nazwa"];
     $rok = date('Y-m-d');
     $godzina = date('H:i');
     mysqli_query($polaczenie,"INSERT INTO `Cobylograne` (`NazwaPios`, `Data`, `Godzina`) VALUES ('".$nazwa."', '".$rok."', '".$godzina."');");
