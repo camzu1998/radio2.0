6025fb84badb70530e1f268839e0c04609dd27b4
camzu98 <kamillanger4@gmail.com>
2017-03-08 17:42:24 +0100

    Delete Ajax Controoler

--- js/ajaxController.js
+++
@@ -1,8 +0,0 @@
-if (typeof XMLHttpRequest == "undefined") {
-    XMLHttpRequest = function() {
-        return new ActiveXObject(
-            navigator.userAgent.indexOf("MSIE 5") >=0 ? "Microsoft.XMLHTTP" : "Msxml2.XMLHTTP"
-        );
-    }
-}
-var xml = new XMLHttpRequest();
