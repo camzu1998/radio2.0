82e09f974a9e2f46d70b0c9423052bfc44ae5d85
camzu98 <kamillanger4@gmail.com>
2017-03-15 10:28:35 +0100

    waszeProp v0

---
+++ waszeProp.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Strona główna</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/snackbar.js"></script>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/snackbar.css">
+</head>
+<body>
+    <div class="container">
+        <div class="twelve columns">
+
+        </div>
+    </div>
+    <footer id="footer">
+        Kamil Langer &copy; kamillanger4@gmail.com
+    </footer>
+</body>
+</html>
